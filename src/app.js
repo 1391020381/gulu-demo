44830cc43181e64daf9cda69c86bcc0a57a72d20
zhoujin <zhoujin@iboxpay.com>
2019-04-04 14:39:27 +0800

    input

--- src/app.js
+++ src/app.js
@@ -2,20 +2,40 @@ import Vue from 'vue'
 import Button from './button.vue'
 import Icon from './icon.vue'
 import ButtonGroup from './button-group'
+import Input from './input'
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group', ButtonGroup)
-
+Vue.component('g-input',Input)
 new Vue({
   el: '#app',
   data: {
     loading1: false,
     loading2: true,
-    loading3: false
+    loading3: false,
+    message:''
   },
-  methods:{
-    log(text){
-     console.log(text)
+  methods: {
+    log(text) {
+      console.log(text)
     }
   }
 })
+
+import chai from 'chai'
+const expect = chai.expect
+
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  vm.$mount('#test')
+  let useElement = vm.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-settings')
+  // vm.$el.remove()
+  // vm.$destroy()
+}
